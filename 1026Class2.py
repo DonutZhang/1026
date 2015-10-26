bf05a9343ef7bdaf254b692dfc8e23aa7112e667
DonutZhang <b10231034@mail.ntust.edu.tw>
2015-10-26 15:06:27 +0800

    test

---
+++ 1026Class2.py
@@ -0,0 +1,83 @@
+
+# coding: utf-8
+
+# In[10]:
+
+#list
+empty_list = [ ]
+weekdays = ['Mon','Tue','Wen']
+type (empty_list)
+
+
+# In[9]:
+
+type(weekdays)
+
+
+# In[12]:
+
+#Tuple
+roles = ('a1','a2','a3')
+type(roles)
+
+
+# In[15]:
+
+weekdays[0] = 'mon is...'
+weekdays
+
+
+# In[16]:
+
+roles[0] = ('A1 is..')
+
+
+# In[17]:
+
+list('中文')
+
+
+# In[20]:
+
+a_tuple = ("M","T","W")
+list (a_tuple)
+
+
+# In[23]:
+
+bir = "1/11/15"
+bir.split('/')
+
+
+# In[24]:
+
+bir = "a/b//c/d//e"
+bir.split('//')
+
+
+# In[30]:
+
+a = [[["1","one","2","two"],"NTUST"],"TAIPEI"]
+a[1]
+
+
+# In[31]:
+
+a[0][0][1]
+
+
+# In[32]:
+
+a[-1]
+
+
+# In[34]:
+
+a[1] = 's'
+a
+
+
+# In[ ]:
+
+
+
---
+++ 1026HW.py
@@ -0,0 +1,41 @@
+
+# coding: utf-8
+
+# In[3]:
+
+hour = 60*60
+print(hour)
+
+
+# In[4]:
+
+seconds_per_hour = hour
+print(seconds_per_hour)
+
+
+# In[7]:
+
+day = seconds_per_hour * 24
+print(day)
+
+
+# In[8]:
+
+seconds_per_day = day
+print(seconds_per_day)
+
+
+# In[9]:
+
+seconds_per_day / seconds_per_hour
+
+
+# In[10]:
+
+seconds_per_day //seconds_per_hour
+
+
+# In[ ]:
+
+
+
